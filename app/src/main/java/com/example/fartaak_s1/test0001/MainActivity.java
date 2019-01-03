67b2867f638292a5e0bc30c8ea9dbbb36eed7906
Morteza1990 <45978664+Morteza1990@users.noreply.github.com>
2019-01-03 19:40:38 +0330

    Initial commit

---
+++ app/src/main/java/com/example/fartaak_s1/test0001/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.fartaak_s1.test0001;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
